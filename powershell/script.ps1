685ab3a0e7a06e78192d4945d8ffe3999b0bf453
~#M?x <89079595+suchsoak@users.noreply.github.com>
2023-10-19 21:06:32 -0300

    script.ps1

--- powershell/script.ps1
+++ powershell/script.ps1
@@ -1,6 +1,6 @@
-$clear
+Clear-Host
 $tx= @"
-___                                             _  _ __  _   
+ ___                                             _  _ __  _   
 | _ \ ___  _ __ __  ___  _ _        ___ __  _ _ (_)| '_ \| |_ 
 |  _// _ \ \ V  V // -_)| '_|      (_-// _|| '_|| || .__/|  _|
 |_|  \___/  \_/\_/ \___||_|        /__/\__||_|  |_||_|    \__|
@@ -9,21 +9,21 @@ ___                                             _  _ __  _
             v:1.0.0
             BY: ~#M?x
 
-            [1] Wsl       [3] Security
-            [2] Firewall  [4]  
+            [1] Wsl             [2] Firewall_rules
+            [3] Browser_Install [4] Drive Reset
 "@
 
 Write-Host $tx
 Start-Sleep -Seconds 1 > $null
 write-Host
-$opt = Read-Host "Choose the option to install:"
+$opt = Read-Host "Choose the option"
 
 switch ($x) {
 
 # Wsl script install
 
 1{
-        $wsl= @"
+$wsl= @"
 __      __ ___  _                        _  _ __  _   
 \ \    / // __|| |          ___ __  _ _ (_)| '_ \| |_ 
  \ \/\/ / \__ \| |__       (_-// _|| '_|| || .__/|  _|
@@ -35,30 +35,15 @@ __      __ ___  _                        _  _ __  _
 Write-Host $wsl
 Start-Sleep -Seconds 2 > $null
 
-try {
-
-    if($false) {
-
-        wsl --install
-    }else {
-    write-host 
-    write-host "Wsl Config"
-    & dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
-    & dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-    & wsl --install
-    & wsl --set-default-version 2
-        
-    }
-    
-    if($true){
+if($true){
     Write-Host "Wsl install"
     wsl --install
     write-host 
     Clear-Host
     write-host "Wsl Config"
-    & dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
-    & dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-    & wsl --set-default-version 2
+     dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+     dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+     wsl --set-default-version 2
 
     Write-Host "Wsl installed"
     Start-Sleep -Seconds 2 > $null
@@ -67,7 +52,7 @@ try {
     do{
         $install= @"
 
-        ___            _          _  _ 
+         ___            _          _  _ 
         |_ _| _ _   ___| |_  __ _ | || |
          | | | ' \ (_-/|  _|/ _` || || |
         |___||_||_|/__/ \__|\__/_||_||_|        
@@ -143,19 +128,12 @@ try {
 
     else {
         
-        & dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
-        & dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-        & wsl --set-default-version 2
-
+         dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+         dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+         wsl --set-default-version 2
     }
+
 }
-catch {
-    Write-Error "Error"
-}
-
-
-
-    }
 
 # Firewall rule
 2{
@@ -174,29 +152,192 @@ catch {
 
 Write-Host $fi
 
-try{
 
-    if ($true){
-    &Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True 
+if ($true){
+    Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True 
     Write-Host " [*] Block 22"
     Write-Host
-    &New-NetFirewallRule -DisplayName "'Block 22'" -Direction Inbound -Protocol TCP -LocalPort 22 -Action Block
+    New-NetFirewallRule -DisplayName "'Block 22'" -Direction Inbound -Protocol TCP -LocalPort 22 -Action Block
     Write-Host
     Write-Host " [*] Block 23"
-    &New-NetFirewallRule -DisplayName "'Block 23'" -Direction Inbound -Protocol TCP -LocalPort 23 -Action Block
+    New-NetFirewallRule -DisplayName "'Block 23'" -Direction Inbound -Protocol TCP -LocalPort 23 -Action Block
     Write-Host " [*] Block 80"
     write-host 
-    &New-NetFirewallRule -DisplayName "'Block 80'" -Direction Inbound -Protocol TCP -LocalPort 80 -Action Block
-    &Get-NetFirewallProfil
+    New-NetFirewallRule -DisplayName "'Block 80'" -Direction Inbound -Protocol TCP -LocalPort 80 -Action Block
+    Get-NetFirewallProfil
 
     }else {
         Write-Host "Haven Problem"
-        write-host "Administrator"
+        write-host "Don't forget by Administrator"
+    }
+}
+
+# Browsers
+
+3{
+
+bs = @"
+___                                  
+| _ ) _ _  ___  _ __ __  ___ ___  _ _ 
+| _ \| '_|/ _ \ \ V  V /(_-// -_)| '_|
+|___/|_|  \___/  \_/\_/ /__/\___||_|  
+Github: https://github.com/schsoak
+BY: ~#M?x
+
+"@
+Start-Sleep 3 > $null
+
+try {
+    Clear-Host
+
+    $install= @"
+
+    ___            _          _  _ 
+   |_ _| _ _   ___| |_  __ _ | || |
+    | | | ' \ (_-/|  _|/ _` || || |
+   |___||_||_|/__/ \__|\__/_||_||_|  
+
+    [*] 1. Brave Browser  [*] 2. Firefox      
+    [*] 3. Chrome         [*] 4. All
+             
+"@
+
+$opt = Read-Host "Choose the option to install:"
+
+
+switch ($opt) {
+    1{
+        brave = @"
+
+         ___                      
+        | _ ) _ _  __ _ __ __ ___ 
+        | _ \| '_|/ _` |\ V // -_)
+        |___/|_|  \__/_| \_/ \___|        
+
+
+"@
+    if($true){
+    winget install --id=Brave.Brave  -e
+    Write-Host
+    Write-Host "Brave Installed"
+
+
+    }else{
+        Write-Host "Warning"
+    }}
+    2{
+    Firefox = @"
+
+     ___  _             __           
+    | __|(_) _ _  ___  / _| ___ __ __
+    | _| | || '_|/ -_)|  _|/ _ \\ \ /
+    |_|  |_||_|  \___||_|  \___//_\_\
+
+"@
+
+    if($true){
+    winget install --id=Mozilla.Firefox  -e
+    }else {
+        Write-Host "Have Problems"
+    }
+    }
+    3{
+    chrom = @"
+    ___  _                          
+    / __|| |_   _ _  ___  _ __   ___ 
+   | (__ |   \ | '_|/ _ \| '  \ / -_)
+    \___||_||_||_|  \___/|_|_|_|\___|
+
+"@
+    if($True){
+        winget install --id=Google.Chrome  -e
+        Write-Host
+        Write-Host "Chrome Installed"
+    }else {
+        Write-Host "Have Problems"
     }
 
-} catch {
-    Write-Host "Haven Problem"
+    }
+    4{
+
+all = @"
+     ___                                  
+    | _ ) _ _  ___  _ __ __  ___ ___  _ _ 
+    | _ \| '_|/ _ \ \ V  V /(_-// -_)| '_|
+    |___/|_|  \___/  \_/\_/ /__/\___||_|  
+    Github: https://github.com/schsoak
+    BY: ~#M?x
+
+"@
+
+if($True){
+    Write-Host "Brave"
+    Start-Sleep 2 > $null
+    winget install --id=BraveSoftware.BraveBrowser -e
+    Clear-Host
+    Write-Host "Firefox"
+    Start-Sleep 2 > $null
+    winget install --id=Mozilla.Firefox -e
+    Clear-Host
+    Write-Host "Chrome"
+    Start-Sleep 2 > $null
+    winget install --id=Google.Chrome -e
+    Clear-Host
+    Write-Host
+    Write-Host "All browsers installed"
+    }else {
+            Write-Host
+            Write-Host "Have Problems"
+        }
+
+    }
+
+    Default {}
 }
 
-   }
+    exit 0
 }
+catch {
+    
+    exit 1
+    
+}
+
+}
+
+4{
+    drives = @"
+     ___        _                      ___                _   
+    |   \  _ _ (_)__ __ ___  ___      | _ \ ___  ___ ___ | |_ 
+    | |) || '_|| |\ V // -_)(_-/      |   // -_)(_-// -_)|  _|
+    |___/ |_|  |_| \_/ \___|/__/      |_|_\\___|/__/\___| \__|
+    Github: https://github.com/schsoak
+    BY: ~#M?x   
+
+"@
+
+try {
+
+    if($true){
+        Write-Host "Drive Reset"
+        dism /online /cleanup-image /scanhealth
+        dism /online /cleanup-image /restorehealth
+        pnputil /resetdrivers /force
+        Write-Host
+        Write-Host "Drives Reset done!"
+        Write-Host 
+        Write-Host "Now reboot your computer"
+
+    }else{
+        Write-Host
+        Write-Host "Haven Problem"
+        Write-Host
+    }
+    
+}
+catch {
+    <#Do this if a terminating exception happens#>
+    Write-Host "Haven Problem, maybe you dont start with administrator"
+}
+    
+}}
