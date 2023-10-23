3b6135a00cf49395401b299b335a60adfccef991
~#M?x <89079595+suchsoak@users.noreply.github.com>
2023-10-23 16:27:27 -0300

    Create netreset.ps1

---
+++ powershell/netreset.ps1
@@ -0,0 +1,35 @@
+clear
+$netreset = @"
+ _  _       _                       _   
+| \| | ___ | |_  _ _  ___  ___ ___ | |_ 
+| .  |/ -_)|  _|| '_|/ -_)(_-// -_)|  _|
+|_|\_|\___| \__||_|  \___|/__/\___| \__|
+
+Github: https://github.com/schsoak
+BY: ~#M?x   
+
+"@
+
+Write-Host $netreset
+
+if ($true){
+
+netsh winsock reset all
+netsh int 6to4 reset all
+netsh int ipv4 reset all
+netsh int ipv6 reset all
+netsh int httpstunnel reset all
+netsh int isatap reset all
+netsh int portproxy reset all
+netsh int tcp reset all
+netsh int teredo reset all
+netsh int ip reset
+netsh interface reset all
+
+Write-Host
+Write-Host "✔ Verification completed!"
+Write-Host
+
+}else {
+    Write-Host " ⚠ Haven Problem, maybe you dont start with administrator ⚠ "
+}
