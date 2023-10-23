ee04fe16364ce57cff4367de52dc7dd0a818f180
~#M?x <89079595+suchsoak@users.noreply.github.com>
2023-10-23 16:28:02 -0300

    Create disk_verefication.ps1

---
+++ powershell/disk_verefication.ps1
@@ -0,0 +1,38 @@
+e$hd = @"
+
+___   _      _                         _   __  _            _    _            
+|   \ (_) ___| |__      __ __ ___  _ _ (_) / _|(_) __  __ _ | |_ (_) ___  _ _  
+| |) || |(_-/| / /      \ V // -_)| '_|| ||  _|| |/ _|/ _` ||  _|| |/ _ \| ' \ 
+|___/ |_|/__/|_\_\       \_/ \___||_|  |_||_|  |_|\__|\__/_| \__||_|\___/|_||_|
+Github: https://github.com/schsoak
+BY: ~#M?x   
+"@
+
+Write-Host $hd
+
+if ($tru){
+Write-Host "[*] Sfc /ScanNow 1/4"
+Start-Sleep 2 > $null
+Sfc /ScanNow
+clear
+Write-Host "[*] dism /online /cleanup-image /scanhealth 2/5"
+Start-Sleep 2 > $null
+dism /online /cleanup-image /scanhealth
+clear
+Write-Host "[*] dism /online /cleanup-image /restorehealth 3/5"
+Start-Sleep 2 > $null
+dism /online /cleanup-image /restorehealth
+clear
+Write-Host "[*] dism /online /cleanup-image /restorehealth 4/5"
+Start-Sleep 2 > $null
+dism /Online /Cleanup-Image /CheckHealth
+clear
+Write-Host "[*] Chkdsk 5/5"
+Start-Sleep 2 > $null
+chkdsk
+clear
+Write-Host "✔ Verification completed!"
+}else {
+    Write-Host
+    Write-Host " ⚠ Verification failed"
+}
