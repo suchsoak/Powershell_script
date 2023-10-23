d1fd2f78d877dbf93a2646b75aa20b61e8ea3c3a
~#M?x <89079595+suchsoak@users.noreply.github.com>
2023-10-23 18:39:08 -0300

    Update disk_verefication.ps1
    Clear-Host and Write-Host

--- powershell/disk_verefication.ps1
+++ powershell/disk_verefication.ps1
@@ -1,4 +1,5 @@
-e$hd = @"
+Clear-Host
+$hd = @"
 
 ___   _      _                         _   __  _            _    _            
 |   \ (_) ___| |__      __ __ ___  _ _ (_) / _|(_) __  __ _ | |_ (_) ___  _ _  
@@ -10,29 +11,29 @@ BY: ~#M?x
 
 Write-Host $hd
 
-if ($tru){
-Write-Host "[*] Sfc /ScanNow 1/4"
-Start-Sleep 2 > $null
-Sfc /ScanNow
-clear
-Write-Host "[*] dism /online /cleanup-image /scanhealth 2/5"
-Start-Sleep 2 > $null
-dism /online /cleanup-image /scanhealth
-clear
-Write-Host "[*] dism /online /cleanup-image /restorehealth 3/5"
-Start-Sleep 2 > $null
-dism /online /cleanup-image /restorehealth
-clear
-Write-Host "[*] dism /online /cleanup-image /restorehealth 4/5"
-Start-Sleep 2 > $null
-dism /Online /Cleanup-Image /CheckHealth
-clear
-Write-Host "[*] Chkdsk 5/5"
-Start-Sleep 2 > $null
-chkdsk
-clear
-Write-Host "✔ Verification completed!"
+if ($true){
+    Write-Host "[*] Sfc /ScanNow 1/4"
+    Start-Sleep 2 > $null
+    Sfc /ScanNow
+    Clear-Host
+    Write-Host "[*] dism /online /cleanup-image /scanhealth 2/5"
+    Start-Sleep 2 > $null
+    dism /online /cleanup-image /scanhealth
+    Clear-Host
+    Write-Host "[*] dism /online /cleanup-image /restorehealth 3/5"
+    Start-Sleep 2 > $null
+    dism /online /cleanup-image /restorehealth
+    Clear-Host
+    Write-Host "[*] dism /online /cleanup-image /restorehealth 4/5"
+    Start-Sleep 2 > $null
+    dism /Online /Cleanup-Image /CheckHealth
+    Clear-Host
+    Write-Host "[*] Chkdsk 5/5"
+    Start-Sleep 2 > $null
+    chkdsk
+    Clear-Host
+    Write-Host "✔ Verification completed!"
 }else {
     Write-Host
     Write-Host " ⚠ Verification failed"
-}
+}}}
