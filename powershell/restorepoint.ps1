ef9407b20e7bac82e4d7e58c454805e9befcb347
~#M?x <89079595+suchsoak@users.noreply.github.com>
2024-01-07 18:32:57 -0300

    Update restorepoint.ps1

--- powershell/restorepoint.ps1
+++ powershell/restorepoint.ps1
@@ -1,69 +1,17 @@
 Clear-Host
-$log =@"
- ___                   _    _          __ _ 
-| __|__ __ ___  _ _  | |_ | |    ___ / _` |
-| _| \ V // -_)| ' \ |  _|| |__ / _ \\__. |
-|___| \_/ \___||_||_| \__||____|\___/|___/ 
-Github:github.com/suchsoak
-BY: suchsok
+    $res =@"
 
-[1] 10    [5] 50    [9]  90
-[2] 20    [6] 60    [10] 100
-[3] 30    [7] 70    [11] 1000
-[4] 40    [8] 80    [12] 10000
+     ___           _                   ___       _        _   
+    | _ \ ___  ___| |_  ___  _ _  ___ | _ \ ___ (_) _ _  | |_ 
+    |   // -_)(_-/|  _|/ _ \| '_|/ -_)|  _// _ \| || ' \ |  _|
+    |_|_\\___|/__/ \__|\___/|_|  \___||_|  \___/|_||_||_| \__|
+    Github:github.com/suchsoak
+    BY: suchsok
+
 "@
 
-Write-Host $log
-
-$get = Read-Host "Put here"
-switch ($get) {
-    1{
-        Clear-Host
-        Get-EventLog -LogName System -Newest 10
-     }
-     2{
-        Clear-Host
-        Get-EventLog -LogName System -Newest 20
-     }
-     3{
-        Clear-Host
-        Get-EventLog -LogName System -Newest 30
-     }
-     4{
-        Clear-Host
-        Get-EventLog -LogName System -Newest 40
-     }
-     5{
-        Clear-Host
-        Get-EventLog -LogName System -Newest 50
-     }
-     6{
-        Clear-Host
-        Get-EventLog -LogName System -Newest 60
-     }
-     7{
-        Clear-Host
-        Get-EventLog -LogName System -Newest 70
-     }
-     8{
-        Clear-Host
-        Get-EventLog -LogName System -Newest 80
-     }
-     9{
-        Clear-Host
-        Get-EventLog -LogName System -Newest 90
-     }
-     10{
-        Clear-Host
-        Get-EventLog -LogName System -Newest 100
-     }
-     11{
-        Clear-Host
-        Get-EventLog -LogName System -Newest 1000
-     }
-     12{
-        Clear-Host
-        Get-EventLog -LogName System -Newest 10000
-     }
-    Default { Write-Host "Invalid Option" }
-}
+Write-Host $res
+$RestorePointName = "MyRestorePoint"
+$Description = "My restore point description"
+New-ComputerRestorePoint -RestorePointType APPLICATION_INSTALL -Description $Description -RestorePointName $RestorePointName
+Get-ComputerRestorePoint -LastStatus
